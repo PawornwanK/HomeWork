560b59ae94f46c3e35369aaafd6e6bebf1848dcf
PawornwanK <67998984+PawornwanK@users.noreply.github.com>
2020-07-08 14:27:09 +0700

    Add files via upload

---
+++ homework.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "   # " <<endl ;
+	cout << "  # # " <<endl ;
+	cout << " #   # " <<endl ;
+	cout << "#     # " <<endl ;
+	cout << "####### " <<endl ;
+	cout << "#     # " <<endl ;
+	cout << "Name : Pawornwan Khiawcha-on " <<endl ;
+	return 0;
+}
